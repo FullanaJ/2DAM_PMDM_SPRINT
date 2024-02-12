6458e5b826e37bbfa531e3e0cdf0b126e5246e4b
jorge <jorgefullana3@gmail.com>
2024-02-12 16:35:34 +0100

    firebase

--- lib/Registros.dart
+++ lib/Registros.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -7,6 +10,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void inicializacion(){
+    await Firebase.initializeApp(
+
+      options: DefaultFirebaseOptions.currentPlatform,
+
+    );
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
